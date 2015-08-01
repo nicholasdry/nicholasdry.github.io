bb9c8202d4eb6dbc35dec66d0f0a57c3086a4cfd
Nicholas Dry <nicholasdry@me.com>
2015-08-01 18:00:09 -0400

    Initial Upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// scripts
+
+function changeImgFlower() {
+	document.getElementById('main-image').src = "img/flower.jpg";
+}
+
+function changeImgHorse() {
+	document.getElementById('main-image').src = "img/horse.jpg";
+}
+
+function changeImgJelly() {
+	document.getElementById('main-image').src = "img/jelly.jpg";
+}
+
+function changeImgPray() {
+	document.getElementById('main-image').src = "img/pray.jpg";
+}
+
+function changeImgStatue() {
+	document.getElementById('main-image').src = "img/statue.jpg";
+}
